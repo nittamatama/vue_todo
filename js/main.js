7a1258a4defc9988a3b186c19a7916977a66c42e
nittamatama <blackmnshin@gmail.com>
2022-07-02 17:04:36 +0900

    v-preディレクティブでマスタッシュを無効化

--- js/main.js
+++ js/main.js
@@ -1,11 +1,6 @@
 const app = Vue.createApp({
   data: () => ({
     message: 'hellovue'
-  }),
-  methods: {
-    clickHandler: function (event) {
-      this.message = this.message.split('').reverse().join('')
-    }
-  }
+  })
 })
 app.mount('#app')
