3e26db2b160afd696e689bcbfab50d4c2ec5e06f
nittamatama <blackmnshin@gmail.com>
2022-07-02 16:48:52 +0900

    reverse使って反転

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const app = Vue.createApp({
   data: () => ({
-
+    message: 'hellovue'
   }),
+  methods: {
+    clickHandler: function (event) {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
 })
 app.mount('#app')
