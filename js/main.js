e614c591e0176ca7efc82bd65d4d6b167fc1401e
nittamatama <blackmnshin@gmail.com>
2022-07-03 12:25:36 +0900

    算出プロパティのゲッターとセッター

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 const app = Vue.createApp({
   data: () => ({
-    message: 'hellovue',
+    basePrice: 100
   }),
   computed: {
-    reversedMessage: function () {
-      return this.message.split('').reverse().join('')
-    }
-  },
-  methods: {
-    reversedMessagemethod: function () {
-      return this.message.split('').reverse().join('')
+    taxIncludedPrice: {
+      get: function () {
+        return this.basePrice * 1.1
+      },
+      set: function (value) {
+        this.basePrice = value / 1.1
+      }
     }
   }
 })
