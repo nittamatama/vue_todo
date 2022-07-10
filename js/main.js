8b982a7e0154aed247e810c5415128fdc7d8bb12
nittamatama <blackmnshin@gmail.com>
2022-07-10 16:46:52 +0900

    v-bindでデータバインディング

--- js/main.js
+++ js/main.js
@@ -1,43 +1,6 @@
 const app = Vue.createApp({
   data: () => ({
-    items: null,
-    keyword: '',
-    message: ''
-  }),
-  watch: {
-    keyword: function (newKeyword, oldKeyword) {
-      console.log(newKeyword)
-      this.debouncedGetAnswer()
-    }
-  },
-  mounted: function() {
-    // this.keyword = 'javascript'
-    // this.getAnswer()
-    this.message = 'waiting for typing'
-    this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
-  },
-  methods: {
-    getAnswer: function () {
-      if(this.keyword === '') {
-        console.log('karamoji')
-        this.items = null
-        return
-      }
-
-      this.message = 'Loading...'
-      const vm = this
-      const params = { page: 1, per_page: 20, query: this.keyword }
-      axios.get('https://qiita.com/api/v2/items', { params }).
-            then(function (response) {
-              vm.items = response.data
-            }).
-            catch(function (error) {
-              vm.message = 'Error' + error
-            }).
-            finally(function () {
-            vm.message = ''
-            })
-    }
-  }
+    isLarge: false
+  })
 })
 app.mount('#app')
