e3befab20d9505a5a2e83f19b2d1281638fd0a62
nittamatama <blackmnshin@gmail.com>
2022-07-10 18:55:11 +0900

    インラインのデータバインディング

--- js/main.js
+++ js/main.js
@@ -1,13 +1,7 @@
 const app = Vue.createApp({
   data: () => ({
-    largeClass: {
-      'large': true,
-      'bg-gray': true
-    },
-    dangerClass: {
-      'text-danger': true
-    },
-    isLarge: true
+    color: 'blue',
+    fontSize: 36
   })
 })
 app.mount('#app')
